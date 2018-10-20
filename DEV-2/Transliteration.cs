c055a1676de5b2044b60822299e2ed45e53e99f9
Hontarau Andrei <gontarev.a.v@tut.by>
2018-10-20 16:37:47 +0300

    Arguments concatenation removed

--- DEV-2/Transliteration.cs
+++ DEV-2/Transliteration.cs
@@ -8,7 +8,6 @@ namespace DEV_2
         static void Main(string[] args)
         {
             System.Console.OutputEncoding = System.Text.Encoding.UTF8;
-            string Sentence;
 
             if (args.Length == 0) //Checking for the presence of arguments
             {
@@ -16,14 +15,10 @@ namespace DEV_2
                 return;
             }
 
-            //Checking for multiple arguments and concatenate if needed
-            ArrayOfStringsConcatenation ArgumentsConcatenator = new ArrayOfStringsConcatenation();
-            Sentence = ArgumentsConcatenator.Concatenate(args);
-
             //Transliteration
             AutoDecidingTransliteration Transliterator = new AutoDecidingTransliteration();
-            Transliterator.Tranliterate(ref Sentence);
-            System.Console.WriteLine(Sentence);
+            Transliterator.Tranliterate(ref args[0]);
+            System.Console.WriteLine(args[0]);
         }
     }
 }
