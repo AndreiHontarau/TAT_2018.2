656f3aebd9300d38218db34213590a58adc5cbfd
Hontarau Andrei <gontarev.a.v@tut.by>
2018-10-27 11:52:28 +0300

    MyList.AddHead() added

--- Lab-2/MyList.cs
+++ Lab-2/MyList.cs
@@ -16,15 +16,36 @@ namespace Lab_2
             Head = headNode;
         }
 
+        public bool IsEmpty()
+        {
+            bool listIsEmpty = false;
+            if (Head.car == null)
+            {
+                listIsEmpty = true;
+            }
+
+            return listIsEmpty;
+        }
+
         public MyList Search()
         {
             MyList SearchResult = new MyList();
             return SearchResult;
         }
 
-        public void AddHead()
+        public void AddHead(Car newCar)
         {
-
+            Node newNode = new Node(newCar);
+            if (this.IsEmpty())
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Head.PrevCar = newNode;
+                newNode.NextCar = Head;
+                Head = newNode;
+            }
         }
     }
 }
